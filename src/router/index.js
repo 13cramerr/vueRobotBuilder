3b6ab0a9ff88e1714ed4183bced84349b4a07285
Ryan Cramer <ah3792@halff.com>
2019-12-02 11:01:55 -0600

    Add sidebars

--- src/router/index.js
+++ src/router/index.js
@@ -4,24 +4,35 @@ import Router from 'vue-router';
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
 import PartInfo from '../parts/PartInfo.vue';
+
 import BrowseParts from '../parts/BrowseParts.vue';
 import RobotArms from '../parts/RobotArms.vue';
 import RobotTorsos from '../parts/RobotTorsos.vue';
 import RobotBases from '../parts/RobotBases.vue';
 import RobotHeads from '../parts/RobotHeads.vue';
 
+import SidebarStandard from '../sidebars/SidebarStandard.vue';
+import SidebarBuild from '../sidebars/SidebarBuild.vue';
+
 Vue.use(Router);
 
 export default new Router({
+  mode: 'history',
   routes: [{
     path: '/',
     name: 'Home',
-    component: HomePage,
+    components: {
+      default: HomePage,
+      sidebar: SidebarStandard,
+    },
   },
   {
     path: '/build',
     name: 'Build',
-    component: RobotBuilder,
+    components: {
+      default: RobotBuilder,
+      sidebar: SidebarBuild,
+    },
   },
   {
     path: '/parts/:partType/:id',
